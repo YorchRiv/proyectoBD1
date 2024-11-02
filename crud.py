0b4f6134bef6b39798539f6a646459f229850bb1
Jorge Mejicanos <jorgemejicanos145@gmail.com>
2024-11-01 23:28:46 -0600

    Lectura Estudiantes

--- crud.py
+++ crud.py
@@ -1,11 +1,11 @@
 def leer_colegiaturas(conn):
     try:
         cursor = conn.cursor()
-        query = "SELECT * FROM CICLOS"
+        query = "SELECT * FROM ESTUDIANTES"
         cursor.execute(query)
         for row in cursor.fetchall():
             print(row)  # Imprime cada fila de resultados
     except Exception as e:
-        print("Error al leer colegiaturas:", e)
+        print("Error al leer datos:", e)
     finally:
         cursor.close()
--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from auth import verificar_usuario
 from db_oracle import conectar_oracle
 from crud import leer_colegiaturas
 
-def limpiar_consola():
-    # Comando para limpiar la consola, funciona en Windows y en Unix
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-# Configura tus parámetros de conexión
+# Credenciales
 server = 'localhost'
 database = 'COLEGIO'
 username = 'USUARIO_DBA'
 password = 'serverSQL'
 connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+def limpiar_consola():
+    # Comando para limpiar la consola, funciona en Windows y en Unix
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 if __name__ == "__main__":
     limpiar_consola()  # Limpia la consola al iniciar
@@ -21,12 +21,12 @@ if __name__ == "__main__":
     print ("Modulo Colegiaturas")
     print ("dev: @yorchriv")
 
-    conn_oracle = conectar_oracle()
-    leer_colegiaturas(conn_oracle)
-    conn_oracle.close()  # Cierra la conexión después de usarla
-
     usuario = input("Ingrese su nombre de usuario: ")
     contraseña = getpass.getpass("Ingrese su contraseña: ")
 
     # Llama a la función para verificar el usuario
-    verificar_usuario(usuario, contraseña, connection_string)
+    if verificar_usuario(usuario, contraseña, connection_string):
+        conn_oracle = conectar_oracle()
+        leer_colegiaturas(conn_oracle)
+        conn_oracle.close()  # Cierra la conexión después de usarla
+        
