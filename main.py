40e922970e04f3f0317e6bd7bcf899d7b1853a6c
Jorge Mejicanos <jorgemejicanos145@gmail.com>
2024-11-01 22:43:47 -0600

    Primer conexion exitosa

---
+++ main.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    usuario = input("Ingrese su nombre de usuario: ")
+    contraseña = input("Ingrese su contraseña: ")
+    
+    # Llama a la función para verificar el usuario
+    verificar_usuario(usuario, contraseña)
